6cb2df4dd52c335bf3a7bfec87ea79fe6d0b8301
Md. Zahidur Rahman <67168046+zahid-bracu@users.noreply.github.com>
2020-11-03 04:31:13 +0600

    up

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+import Forms from './Components/Forms';
+import Banner from './Components/Banner';
+import './App.css';
+import React, {createContext, useState} from 'react';
+
+export const information = React.createContext();
+
+function App() {
+  const [info,setInfo]=useState(false)
+   
+  return (
+    <information.Provider value={[info,setInfo]}>
+      <div className="main">
+        <Header></Header>
+        <Banner></Banner>
+        <Forms></Forms>
+      </div>
+    </information.Provider>
+    
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,31 @@
+import React, {useState,useContext} from 'react';
+import { Button, Nav, Navbar, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import {information} from '../App';
+const Header = () => {
+    const [info,setInfo]=useContext(information);
+    console.log(info);
+    return (
+        <Navbar bg="light" expand="lg">
+        <div className="container">
+        <Navbar.Brand href="#home">Firebase Form</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            <Nav.Link target="_blank" href="https://portfolio-zahid-bracu.netlify.app/">Portfolio</Nav.Link>
+            <Nav.Link target="_blank" href="https://github.com/zahid-bracu">Github</Nav.Link>
+            {
+                !info && <Nav.Link href="#link" active>Login</Nav.Link>
+            }
+            
+            </Nav>
+            <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+            </Form>
+        </Navbar.Collapse>
+        </div>
+        </Navbar>
+    );
+};
+
+export default Header;
